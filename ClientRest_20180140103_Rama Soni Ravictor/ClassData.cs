d92523df116c9f01a4e9b00eca50a87b5860a7c6
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-12-21 13:40:58 +0700

    Add project files.

---
+++ ClientRest_20180140103_Rama Soni Ravictor/ClassData.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ClientRest_20180140103_Rama_Soni_Ravictor
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
